27817826dcb7ee5fc267fc62ca366c66de08de26
Domjl <domjdragon2@gmail.com>
2016-04-21 15:58:28 -0700

    Update sort.cpp
    Messed around.. don't like it.

--- sort.cpp
+++ sort.cpp
@@ -1,1 +1,22 @@
-//Full Recursive Sort
+//Full Recursive Sort **HEAVY USAGE**
+//NOPE, don't like it... 
+BookNode recursiveSort(BookNode *n)
+{
+  if(n->next == null)
+  {
+    return n;
+  }
+  else if(n.get___() > recursiveSort(n->next))
+  {
+    swapUp(n);
+  }
+} 
+
+swapUp(BookNode *n)
+{
+  BookNode s = n->next;
+  
+}
+
+//Insertion Sort 
+//Assumed to be already sorted.
