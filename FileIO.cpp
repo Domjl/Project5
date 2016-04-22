ee7c2ecf713e2563c8a1838ced63a74b4c311a9f
NatasAuLait <oliver.scheiwiller@gmail.com>
2016-04-22 14:02:55 -0700

    Added files via upload
    extracted read and write into separate methods that take filename as a parameter. Currently still displays read txt file on console, next step is to store in variable and create linked list with these values.

--- FileIO.cpp
+++ FileIO.cpp
@@ -8,32 +8,60 @@ using std::endl;
 using std::cout;
 using std::cerr;
 
+int readData(string fileName);
+int writeData(string fileName);
+
 int main() {
-	// Path to file to be opened (this is also the file we will write out to)
-	string fileName = "/Users/Natas/Dropbox/School/CSE 220/ShoppingSystem/bookFile.txt";
+
+	string fileName = "/Users/Natas/Dropbox/School/CSE 220/ShoppingSystem/PolymorphismBooklist.txt";
+
+	if(readData(fileName) == 0) {
+		cout << "Success Reading File" << endl;
+	}
+	if(writeData(fileName) == 0) {
+		cout << "Success Writing File" << endl;
+	}
+	
+	return 0;
+}
+
+int readData(string fileName) {
+	// Path to file to be opened
+	string file = fileName;
 	
 	// Open a stream with the file provided above
-	ifstream fin(fileName);
+	ifstream fin(file);
 	// check to make sure file is found, otherwise create an error message and exit
 	if (!fin) {
-		cerr << "File " << fileName << " not found." << endl;
+		cerr << "File " << file << " not found." << endl;
 		return -1;
 	}
-	// open the outstream for where the output should be written, in append mode to add to end
-	ofstream fout(fileName, ios::app);
 
-	char str[50];
+	char str[100];
 
 	// read in entire text file and output to console
 	while (fin) {
-		fin.getline(str, 50);
+		fin.getline(str, 100);
 		if (fin) cout << str << endl;
 	}
 
-	fout << "\n\nTest Book Title\n" << "10\n" << "$72.99\n" << "2000\n\n";
-
 	// close both streams before exiting
 	fin.close();
+
+	return 0;
+}
+
+int writeData(string fileName) {
+	// Path to file
+	string file = fileName;
+
+	// open the outstream for where the output should be written, in append mode to add to end
+	ofstream fout(fileName, ios::app);
+
+	// test write out into file
+	fout << "\n\nTest Book Title\n" << "10\n" << "$72.99\n" << "2000\n\n";
+
 	fout.close();
+
 	return 0;
 }
