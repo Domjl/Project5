7fedfdcb72cfd56496e13361c8b949fec8cf24de
Domjl <domjdragon2@gmail.com>
2016-04-23 14:42:47 -0700

    Added sort()
    Appended sort method to end of main.cpp

--- main.cpp
+++ main.cpp
@@ -364,3 +364,65 @@ void destroyCatlog(BookNode* & node){
         }
     }
 }
+
+/**
+ * Purpose: sort.. that I believe based on my understanding of c++ will work.
+ * with comments.. :D
+ * 
+ * @param *head pass in the current head of the list
+ * @return returns the new head of the list.
+ */
+BookNode sort(BookNode *head)       
+{
+	BookNode *newhead = nullptr;      // Head to be.
+	BookNode *current = nullptr;      // Position place holder for where in the sorted list we are.
+
+	while(head->next != nullptr)      // repeat until only head remains
+	{
+		BookNode *n = head;             // temp node for looping through
+		BookNode *max = n;              // max node
+		BookNode *maxParent = n;        // max node's parent
+
+		while(n->next != nullptr)       // while has another node
+		{
+			if(n->next->getAmount() > n->getAmount()) // if the next node is larger than the current node
+			{
+				max = n->next;              // current max is the next node
+				maxParent = n;              // current node is the max's parent 
+			}
+		}
+
+                // remove the node - Delink the node from the link and relink the remaining list
+		if(max == head)                 // if max is first element
+		{
+			head = head->next;            // set head to next node
+		}
+		else                            //
+		{
+			maxParent->next = max->next;  // max's parent's next == max's next node
+		}
+		max->next = nullptr;            // max's next beocmes null
+		
+		//Append the cut node to the new list.
+		if(current == nullptr)          // if newhead hasn't beeen set yet
+		{
+		  newhead = max;                 // set a starting point
+		  current = newhead;            // safety
+		}
+		else
+		 {
+		   current->next = max;         // next max is the max we found
+		   current = max;                // move along in the sorted list
+		 }
+		 
+		 /**
+		  *  catch. if(current->next != nullptr) throw error;
+		  */
+		  
+	}
+
+	current->next = head;             // add the remaining value;
+	//current = nullptr; 		 // might break things, my attempt to deallocate the "current" pointer.
+	
+	return newhead;                   // return the new list
+}
